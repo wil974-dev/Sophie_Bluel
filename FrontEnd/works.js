0dfbd43fb2c2e06ccdfe4fdb5f1bec91f2d71e10
wil974-dev <wggron99@gmail.com>
2025-01-27 16:08:56 +0400

    Ajout des boutons de filtres et du tri dynamique

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -8,7 +8,7 @@ async function getWorks(){
         const reponse = await fetch("http://localhost:5678/api/works");
 
         if(!reponse.ok){
-            throw new Error('Erreur HTTP : ${reponse.status}');
+            throw new Error(`Erreur HTTP : ${reponse.status}`);
         }
 
         const works = await reponse.json();
@@ -22,19 +22,43 @@ async function getWorks(){
 
 }
 
+/**
+ * Récupère les categories des travaux et crée les boutons
+ * avec la fonction createButtonFilter().
+ */
+async function getCategory(){
+    try {
+        const reponse = await fetch("http://localhost:5678/api/categories");
+
+        if(!reponse.ok){
+            throw new Error(`Erreur HTTP : ${reponse.status}`);
+        }
+
+        let categories = await reponse.json();
+        const allCategory = {name:"Tous"};
+        
+        categories = [allCategory, ...categories];
+
+        createButtonFilter(categories);
+    }
+    catch(error){
+        console.error("Erreur pour la récupération des catégories : ", error.message);
+    }
+    
+}
 
 
-getWorks();
 
-// fonctions
 
 /**
- * Affiche tous les travaux dans la class gallery.
- * @param {Array} works - Représente les travaux récupérer sur le serveur.
+ * Efface les éléments et affiche tous les travaux dans la class gallery.
+ * @param {Array} works - Représente les travaux.
  */
 function showWorks(works){
+    const galleryElement = document.querySelector(".gallery");
+    galleryElement.innerHTML = "";
+
     for(let i = 0; i < works.length; i++){
-        const galleryElement = document.querySelector(".gallery");
         const figureElement = document.createElement("figure");
         const imgElement = document.createElement("img");
         const figcaptionElement = document.createElement("figcaption");
@@ -48,3 +72,60 @@ function showWorks(works){
     }
 }
 
+/**
+ * Créer un bouton filtre avec un évènement contenant une fonction de tri filterWorks.
+ * @param {array} category - Correspond à une categorie.
+ */
+function createButtonFilter(category){
+    
+    const filterElement = document.querySelector(".filter");
+    
+    category.forEach(category => {
+        const inputElement = document.createElement("input");
+
+        inputElement.type = "button";
+        inputElement.value = category.name;
+        
+        inputElement.addEventListener("click", () => {
+            if(category.name === "Tous") {
+                getWorks();
+            }
+            else{
+                filterWorks(category.name);
+            }
+        });
+
+        filterElement.appendChild(inputElement);
+
+    });
+
+}
+
+/**
+ * Récupère les travaux et affichent ceux qui corresponde à la catégorie.
+ * @param {string} categoryName - la catégorie.
+ */
+async function filterWorks(categoryName){
+    try{
+        const reponse = await fetch("http://localhost:5678/api/works");
+
+        if(!reponse.ok){
+            throw new Error(`Erreur HTTP : ${reponse.status}`);
+        }
+
+        const works = await reponse.json();
+
+        const listeWorks = works.filter(work => work.category.name === categoryName);
+
+        const galleryElement = document.querySelector(".gallery");
+        
+        showWorks(listeWorks);
+    }
+    catch{
+        console.error("Erreur pour la récupération des travaux : ", error.message);
+    }
+}
+
+
+getWorks();
+getCategory();
